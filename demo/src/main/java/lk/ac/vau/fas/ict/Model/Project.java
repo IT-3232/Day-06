5a372afd5dcec6d587b88f1f6fdb89b5cf1f7e1e
Thilankavishka <thilankavishka25@gmail.com>
2025-04-25 16:14:40 +0530

    Add Project Model Codes

--- demo/src/main/java/lk/ac/vau/fas/ict/Model/Project.java
+++ demo/src/main/java/lk/ac/vau/fas/ict/Model/Project.java
@@ -1,5 +1,24 @@
 package lk.ac.vau.fas.ict.Model;
 
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Project {
-    
+   @Id
+   private int project_id;
+   private String name;
+   private long totalcost;
+   
+   
+   
+   @ManyToMany(mappedBy = "projects")
+   private List<Employee> employees;
+ 
 }
