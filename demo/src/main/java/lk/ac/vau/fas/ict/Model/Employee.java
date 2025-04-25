972e0a59892c4c54194c07bc74ffa3a205473109
Thilankavishka <thilankavishka25@gmail.com>
2025-04-25 16:13:20 +0530

    Add Employee Codes

--- demo/src/main/java/lk/ac/vau/fas/ict/Model/Employee.java
+++ demo/src/main/java/lk/ac/vau/fas/ict/Model/Employee.java
@@ -1,5 +1,39 @@
 package lk.ac.vau.fas.ict.Model;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+
+@Entity
 public class Employee {
+     @Id
+     private String empNo;
+     private String name;
+     private int age;
+     private double salary;
+     
+     
+     @ManyToOne
+     @JoinColumn(name = "dept_id")
+     private Department department; 
+     
     
+     @ManyToMany
+     @JoinTable(
+         name = "employee_project",
+         joinColumns = @JoinColumn(name = "emp_no"),
+         inverseJoinColumns = @JoinColumn(name = "project_id")
+     )
+     private List<Project> projects;
+     
+     
+     
+
 }
